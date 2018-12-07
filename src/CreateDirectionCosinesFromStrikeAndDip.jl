d599ebfca3d24aa7dbb4c65e018a4e0a0446566e
Tim Davis <davis@gfz-potsdam.de>
2018-12-07 10:17:09 +0100

    Linking to module name in function not required

--- src/CreateDirectionCosinesFromStrikeAndDip.jl
+++ src/CreateDirectionCosinesFromStrikeAndDip.jl
@@ -1,13 +1,13 @@
 function CreateDirectionCosinesFromStrikeAndDip(Strike,Dip)
 #Given strike and dip in degrees create direction cosines of the fault (normal,dip and strike). 
 
-( StrikeSlipCosine,DipSlipCosine ) = MyModule.CalculateDSandSSDirs( cosd(Dip),0,cosd(Dip) ) #Plane dipping at dip (facing east!)
+( StrikeSlipCosine,DipSlipCosine ) = CalculateDSandSSDirs( cosd(Dip),0,cosd(Dip) ) #Plane dipping at dip (facing east!)
 RAngle=deg2rad(-Strike)
-StrikeSlipCosine=MyModule.RotateCosine3D(StrikeSlipCosine,RAngle,"z")
-DipSlipCosine=MyModule.RotateCosine3D(DipSlipCosine,RAngle,"z")
+StrikeSlipCosine=RotateCosine3D(StrikeSlipCosine,RAngle,"z")
+DipSlipCosine=RotateCosine3D(DipSlipCosine,RAngle,"z")
 FaceNormalVector=cross(vec(StrikeSlipCosine),vec(DipSlipCosine))
 FaceNormalVector=FaceNormalVector'
-MyModule.CheckDirectionCosinePerp(FaceNormalVector,DipSlipCosine,StrikeSlipCosine)
+CheckDirectionCosinePerp(FaceNormalVector,DipSlipCosine,StrikeSlipCosine)
 
 return(StrikeSlipCosine,DipSlipCosine,FaceNormalVector)
 
