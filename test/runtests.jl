4cc8b73642d19b6067f25f96e7eb742f611bc21b
Tim Davis <davis@gfz-potsdam.de>
2018-12-07 16:06:37 +0100

    Added new test to check OkadaVsLine dislocations

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,3 +13,8 @@ println("Starting Test 2")
 ##Test 2, Check the LD's match glide dislocation formulas
 include("test_LDvsBarberGlideDisc.jl")
 println("Test 2 passed")
+
+println("Starting Test 3")
+##Test 3, Check the LD's Halfspace match Okada disc at ground surface.
+include("test_LDvsOkada.jl")
+println("Test 3 passed")
--- test/test_LDvsOkada.jl
+++ test/test_LDvsOkada.jl
@@ -1,4 +1,4 @@
-#Test case comparing Okada dislocations with TDs (displacement)
+#Test case comparing Okada dislocations with LDs (displacement). Checks the half space displacement eqs are good. 
 
 #Start creating vars for function: 
 println("creating func vars")
@@ -6,39 +6,32 @@ println("creating func vars")
 #Inputs
 
 #Fault geom
-Strike=90;
-Dip=45;
-Length=1000;
+Strike=0;
+Dip=0;
+Length=100000;
 Width=5; # fault width in the DIP direction (WIDTH > 0)
 TipDepth=1;
 Rake=90;
 #Fault slip
-Dds=1;
-Dn=0.5;
+Dds=0.1;
+Dn=0.1;
 Dss=0;
 #Elastic cons
 nu=0.25;
+mu=5;
 
 #Dip relative to X-axis
-Beta=rad2deg(90-Dip);
-a=Width;
+Beta=deg2rad(180-Dip);
+a=Width/2;
 Ds=Dds;
 #Depth of midpoint
 MidDeptha=sind(Dip)*Width;
 MidDepth=MidDeptha/2+TipDepth;
 
-
 # Start some vectors (spaced points)
-x = range(-10,stop=10,length=50); #linspace deprecated
-(x,y)=MyModule.meshgrid(x,x);
-z=ones(size(x))*-0; #Ground surface
-
-#Get lengths (for reshapes later)
-dimx,dimy = size(x);
-#Turn to col vectors
-x=reshape(x,length(x),1);
-y=reshape(y,length(y),1);
-z=reshape(z,length(z),1);
+x = [-10:0.1:10;]
+y=zeros(length(x))
+z=zeros(length(x))
 
 DispFlag=1;
 StressFlag=1;
@@ -46,56 +39,37 @@ HSflag=1;
 
 println("Vars created -> to LD func")
 
-(SxxDs,SyyDs,SxyDs,SxxDn,SyyDn,SxyDn,UxDs,UxDn,UyDs,UyDn)=MyModule.LD(x,y,0,MidDepth,a,Beta,Ds,Dn,nu,mu,DispFlag,StressFlag,HSflag)
+(SxxDs,SyyDs,SxyDs,SxxDn,SyyDn,SxyDn,UxDs,UxDn,UyDs,UyDn)=MyModule.LD(x,z,0,-MidDepth,a,Beta,Ds,Dn,nu,mu,DispFlag,StressFlag,HSflag)
 #Accumulating arrays
-(sXX,sYY,sXY,uX,uY)=MyModule.LD_sum(SxxDs,SxxDn,SyyDs,SyyDn,SxyDs,SxyDn,UxDs,UxDn,UyDs,UyDn)
+(sXX,sYY,sXY,uX,uZ)=MyModule.LD_sum(SxxDs,SxxDn,SyyDs,SyyDn,SxyDs,SxyDn,UxDs,UxDn,UyDs,UyDn)
 
 println("Out of func, too Okada")
 
 
 println("Into Okada func")
-(uX,uY,uZ)=MyModule.Okada1985RectangularDislocation(x,y,MidDepth,Strike,Dip,Length,Width,Rake,Dds,Dn,nu);
+(Ux,Uy,Uz,unn,une,uen,uee)=MyModule.Okada1985RectangularDislocation(x,y,MidDepth,Strike,Dip,Length,Width,Rake,Dds,Dn,nu);
 
 println("Out of func, drawing time, start by reshape")
 
 
 UxRes=maximum(Ux[:].-uX[:]);
-UyRes=maximum(Uy[:].-uY[:]);
+#UyRes=maximum(Uy[:].-uY[:]);
 UzRes=maximum(Uz[:].-uZ[:]);
 
+#close()
+#using PyPlot
+#scatter(x,Uz)
+#scatter(x,uZ)
+
 println("Values of residuals: TDE vs Okada")
-@info UxRes UyRes UzRes #Display values in test output
-if UxRes>1E-14
-	error("UxRes too high, Okada and TD not matching for displacement")
+@info UxRes UzRes #Display values in test output
+if UxRes>1E-7
+	error("UxRes too high, Okada and LD not matching for ground displacement")
 end
-if UyRes>1E-14
-	error("UyRes too high, Okada and TD not matching for displacement")
-end
-if UzRes>1E-14
-	error("UzRes too high, Okada and TD not matching for displacement")
+if UzRes>1E-7
+	error("UzRes too high, Okada and LD not matching for ground displacement")
 end
 
 
 println("Test Passed")
-
-# #= if you want to draw remove this line
-# x=reshape(x,dimx,dimy);
-# y=reshape(y,dimx,dimy);
-# uX=reshape(uX,dimx,dimy);
-# Ux=reshape(Ux,dimx,dimy);
-# uY=reshape(uY,dimx,dimy);
-# Uy=reshape(Uy,dimx,dimy);
-# uZ=reshape(uZ,dimx,dimy);
-# Uz=reshape(Uz,dimx,dimy);
-
-# Value=uZ
-# using NaNMath
-# Top=maximum([NaNMath.maximum(Value),abs(NaNMath.minimum(Value))])
-# steps=10; #Steps from centre to top. 
-# levels = [-Top:Top/steps:Top;]
-# using PyPlot
-# close()
-# contourf(x,y,Value, levels=levels);
-# cbar = colorbar()
-# =#
 
