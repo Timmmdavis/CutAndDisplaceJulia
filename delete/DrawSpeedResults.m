a0374e6cea48b997d9e1d5d1c1c59d58b3fbbb5d
Tim Davis <davis@gfz-potsdam.de>
2019-01-09 17:24:24 +0100

    DrawingASpeedComparisonDiagram

---
+++ delete/DrawSpeedResults.m
@@ -0,0 +1,17 @@
+FS.NoTris=[2,10,100,2500];
+FS.MATLAB=[0.013,0.072,0.907,186.39];
+FS.Julia=[0.005,0.013,0.104,2.534];
+
+HS.NoTris=[2,10,100,2500];
+HS.MATLAB=[0.37,1.69,17.12,425.94];
+HS.Julia=[0.06,0.12,0.87,21.38];
+
+loglog(FS.NoTris,FS.MATLAB)
+hold on
+loglog(FS.NoTris,FS.Julia)
+loglog(HS.NoTris,HS.MATLAB)
+loglog(HS.NoTris,HS.Julia)
+
+xlabel('NoTris')
+ylabel('Time(s)')
+legend MATLAB-FS Julia-FS MATLAB-HS Julia-HS
