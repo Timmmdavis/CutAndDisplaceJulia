9de645b977145303b601d0c12773a84260051a83
Tim Davis <davis@gfz-potsdam.de>
2019-01-17 11:33:53 +0100

    Fix errors if only one type is computed in TD/LD.

--- src/LD_sum.jl
+++ src/LD_sum.jl
@@ -5,12 +5,23 @@ function LD_sum(SxxDs,SxxDn,SyyDs,
 #Simply sums the different modes of dislocations. (Safer than always writing this out)
 #Could be written more elegantly 
 
-#Accumulating arrays
-Sxx=sum(SxxDs.+SxxDn,dims=2);
-Syy=sum(SyyDs.+SyyDn,dims=2);
-Sxy=sum(SxyDs.+SxyDn,dims=2);
-Ux=sum(UxDs.+UxDn,dims=2);
-Uy=sum(UyDs.+UyDn,dims=2);
+if isempty(Sxx)
+	Sxx=SxxDs;
+	Syy=SyyDs;
+	Sxy=SxyDs;
+else
+	Sxx=sum(SxxDs.+SxxDn,dims=2);
+	Syy=sum(SyyDs.+SyyDn,dims=2);
+	Sxy=sum(SxyDs.+SxyDn,dims=2);
+end
+
+if isempty(Sxx)
+	Ux=UxDs;
+	Uy=UyDs;
+else
+	Ux=sum(UxDs.+UxDn,dims=2);
+	Uy=sum(UyDs.+UyDn,dims=2);
+end
 
 return(Sxx,Syy,Sxy,Ux,Uy)
 
--- src/TD_sum.jl
+++ src/TD_sum.jl
@@ -7,17 +7,33 @@ function TD_sum(ExxDn, EyyDn, EzzDn, ExyDn, ExzDn, EyzDn,
 #Simply sums the different modes of dislocations. (Safer than always writing this out)
 #Could be written more elegantly 
 
-#Accumulating arrays
-Exx=sum(ExxDn+ExxDss+ExxDds,dims=2);
-Eyy=sum(EyyDn+EyyDss+EyyDds,dims=2);
-Ezz=sum(EzzDn+EzzDss+EzzDds,dims=2);
-Exy=sum(ExyDn+ExyDss+ExyDds,dims=2);
-Exz=sum(ExzDn+ExzDss+ExzDds,dims=2);
-Eyz=sum(EyzDn+EyzDss+EyzDds,dims=2);
-Ux=sum(UxDn+UxDss+UxDds,dims=2);
-Uz=sum(UzDn+UzDss+UzDds,dims=2);
-Uy=sum(UyDn+UyDss+UyDds,dims=2);
 
+if isempty(ExxDn)
+	Exx=ExxDn;
+	Eyy=EyyDn;
+	Ezz=EzzDn;
+	Exy=ExyDn;
+	Exz=ExzDn;
+	Eyz=EyzDn;
+else
+	#Accumulating arrays
+	Exx=sum(ExxDn+ExxDss+ExxDds,dims=2);
+	Eyy=sum(EyyDn+EyyDss+EyyDds,dims=2);
+	Ezz=sum(EzzDn+EzzDss+EzzDds,dims=2);
+	Exy=sum(ExyDn+ExyDss+ExyDds,dims=2);
+	Exz=sum(ExzDn+ExzDss+ExzDds,dims=2);
+	Eyz=sum(EyzDn+EyzDss+EyzDds,dims=2);
+end
+
+if isempty(UxDn)
+	Ux=UxDn;
+	Uz=UzDn;
+	Uy=UyDn;
+else
+	Ux=sum(UxDn+UxDss+UxDds,dims=2);
+	Uz=sum(UzDn+UzDss+UzDds,dims=2);
+	Uy=sum(UyDn+UyDss+UyDds,dims=2);
+end
 
 
 return(Exx,Eyy,Ezz,Exy,Exz,Eyz,Ux,Uy,Uz)
