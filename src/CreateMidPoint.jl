76ca97f910621c4e0a12a0617e99bfc3ccb4d73a
Tim Davis <davis@gfz-potsdam.de>
2019-02-13 12:28:15 +0100

    Fixed compile error (used single quotation mark by mistake)

--- src/CreateMidPoint.jl
+++ src/CreateMidPoint.jl
@@ -5,7 +5,7 @@ function CreateMidPoint(Pa,Pb,Pc)
     a=sqrt((Pb[1]-Pc[1])^2+(Pb[2]-Pc[2])^2+(Pb[3]-Pc[3])^2);
     b=sqrt((Pa[1]-Pc[1])^2+(Pa[2]-Pc[2])^2+(Pa[3]-Pc[3])^2);
     if a==0 || b==0 || c==0
-        error('Slither triangles or triangles with no area exist on your surface')
+        error("Slither triangles or triangles with no area exist on your surface")
     end
     #Calculating midpoint using
     #http://mathworld.wolfram.com/Incenter.html
