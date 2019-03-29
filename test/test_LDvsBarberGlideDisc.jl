3d2a0d753ee32e2737f357db3f4029a3fc306806
Tim Davis <davis@gfz-potsdam.de>
2019-03-29 19:08:01 +0100

    Update, git ignore on new surface... Irk

--- test/test_LDvsBarberGlideDisc.jl
+++ test/test_LDvsBarberGlideDisc.jl
@@ -27,64 +27,83 @@ Dn=[0. 0.];
 
 DispFlag=1;
 StressFlag=1;
-HSflag=0;
+HSFlag=0;
 
 println("Vars created -> to LD func")
 
-(SxxDs,SyyDs,SxyDs,SxxDn,SyyDn,SxyDn,UxDs,UxDn,UyDs,UyDn)=CutAndDisplaceJulia.LD(X[:],Y[:],xe,ye,hlflen,Beta,Ds,Dn,ν,G,DispFlag,StressFlag,HSflag)
+
+
+(σxxDs,σyyDs,σxyDs,σxxDn,σyyDn,σxyDn,UxDs,UxDn,UyDs,UyDn)=CutAndDisplaceJulia.LD(X[:],Y[:],xe,ye,hlflen,Beta,Ds,Dn,ν,G,DispFlag,StressFlag,HSFlag)
 #Accumulating arrays
-(sXX,sYY,sXY,uX,uY)=CutAndDisplaceJulia.LD_sum(SxxDs,SxxDn,SyyDs,SyyDn,SxyDs,SxyDn,UxDs,UxDn,UyDs,UyDn)
+(σxx,σyy,σxy,Ux,Uy)=CutAndDisplaceJulia.LD_sum(σxxDs,σxxDn,σyyDs,σyyDn,σxyDs,σxyDn,UxDs,UxDn,UyDs,UyDn)
 
 println("Out of func, too Barber1992_GlideDislocation")
-
-(X,Y,Sxx,Syy,Sxy,Ux,Uy)=CutAndDisplaceJulia.Barber1992_GlideDislocation(G,X[:],Y[:],a,Ds[1],ν)
+(X,Y,σxxAn,σyyAn,σxyAn,UxAn,UyAn)=CutAndDisplaceJulia.Barber1992_GlideDislocation(G,X[:],Y[:],a,Ds[1],ν)
 
 
 println("Out of func, drawing time, start by reshape")
 
-UxRes=maximum(filter(!isnan, Ux[:].-uX[:]));
-UyRes=maximum(filter(!isnan, Uy[:].-uY[:]));
-SxxRes=maximum(filter(!isnan, Sxx[:].-sXX[:]));
-SyyRes=maximum(filter(!isnan, Syy[:].-sYY[:]));
-SxyRes=maximum(filter(!isnan, Sxy[:].-sXY[:]));
+UxRes=maximum(filter(!isnan, UxAn[:].-Ux[:]));
+UyRes=maximum(filter(!isnan, UyAn[:].-Uy[:]));
+σxxRes=maximum(filter(!isnan, σxxAn[:].-σxx[:]));
+σyyRes=maximum(filter(!isnan, σyyAn[:].-σyy[:]));
+σxyRes=maximum(filter(!isnan, σxyAn[:].-σxy[:]));
 
 println("Values of residuals: LD vs Barber")
-@info UxRes UyRes SxxRes SyyRes SxyRes
+@info UxRes UyRes σxxRes σyyRes σxyRes
 if UxRes>1E-13
 	error("UxRes too high, LD and glide dislocation not matching for displacement")
 end
 if UyRes>1E-13
 	error("UyRes too high, LD and glide dislocation not matching for displacement")
 end
-if SxxRes>1E-12
+if σxxRes>1E-12
 	error("SxxRes too high, LD and glide dislocation not matching for stress")
 end
-if SyyRes>1E-12
+if σyyRes>1E-12
 	error("SyyRes too high, LD and glide dislocation not matching for stress")
 end
-if SxyRes>1E-12
+if σxyRes>1E-12
 	error("SxyRes too high, LD and glide dislocation not matching for stress")
 end
 
-
 println("Test Passed")
 
-# ##= if you want to draw remove this line
-# x=reshape(X,dimx,dimy);
-# y=reshape(Y,dimx,dimy);
-# uX=reshape(uX,dimx,dimy);
-# Ux=reshape(Ux,dimx,dimy);
-# uY=reshape(uY,dimx,dimy);
-# Uy=reshape(Uy,dimx,dimy);
-
-# Value=uX
-# using NaNMath
-# Top=maximum([NaNMath.maximum(Value),abs(NaNMath.minimum(Value))])
-# steps=10; #Steps from centre to top. 
-# levels = [-Top:Top/steps:Top;]
-# using PyPlot
-# close()
-# contourf(x,y,Value, levels=levels);
-# cbar = colorbar()
-# ##=#
+#=
+#Draw
+struct PlotAn;	σxx;σyy;σxy; end
+struct PlotNum;	σxx;σyy;σxy; end
+AnalyticalResults=PlotAn(σxxAn,σyyAn,σxyAn)
+NumericalResults=PlotNum(σxx,σzz,σxz)
+#Get fields in structure
+FieldsInStructAn=fieldnames(typeof(AnalyticalResults));
+FieldsInStructNum=fieldnames(typeof(NumericalResults));
+for i=1:length(FieldsInStructAn)
+	AnStr=FieldsInStructAn[i];
+	NumStr=FieldsInStructNum[i];
+	println(AnStr)
+	AnVal=getfield(AnalyticalResults, FieldsInStructAn[i])
+	NumVal=getfield(NumericalResults, FieldsInStructNum[i])
+	min = minimum(AnVal[:]);
+	max = maximum(AnVal[:]);
+	NumValue=reshape(NumVal,size(xobs))
+	AnValue=reshape(AnVal,size(xobs))
+	x=xobs;
+	y=yobs;
+	using NaNMath
+	steps=10; #Steps from centre to top. 
+	Spacing=(max-min)./10;
+	levels = [min:Spacing:max;]
+	using PyPlot
+	figure()
+	plt.subplot(211)
+	contourf(x,y,NumValue, levels=levels);
+	cbar = colorbar();
+	plt.title("Numerical Result $AnStr");
+	plt.subplot(212)
+	contourf(x,y,AnValue, levels=levels);
+	cbar = colorbar();
+	plt.title("Analytical Result $NumStr")
+end
+ =#
 
