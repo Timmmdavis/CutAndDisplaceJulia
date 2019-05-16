4c17ddac03dbce531999b827f268a80883dee762
Tim Davis <davis@gfz-potsdam.de>
2019-05-16 14:08:13 +0200

    updates to manifest to include "Printf" package

--- src/xyzExport.jl
+++ src/xyzExport.jl
@@ -1,11 +1,23 @@
 function xyzExport(x,y,z,filename)
 #vectors x y and z
 
-#using DelimitedFiles
-writedlm("$filename.xyz",[x y z]," ")
+#using Printf
+n_points=length(x)
+#create a new file for writing
+io = open("$filename.off", "w+");
+for i=1:n_points
+
+	#13 digits precision
+	@printf(io,"%0.13f", x[i]);print(io," ");
+	@printf(io,"%0.13f", y[i]);print(io," ");
+	@printf(io,"%0.13f", z[i]);print(io,"\r\n");
+
+end
+
+close(io);
 
 #OutputDir
-OutputDirectory=string(pwd(),"\\",filename,".xyz")
+OutputDirectory=string(pwd(),"\\",filename,".off")
 
 return OutputDirectory
 end
