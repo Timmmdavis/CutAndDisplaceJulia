62c5539904e4a3cce59e3eb84c677c0ccd09a146
Tim Davis <davis@gfz-potsdam.de>
2019-09-09 16:21:26 +0200

    updates so i remember what to call the crack extension force (G-criterion)

--- src/StrainEnergyRelease.jl
+++ src/StrainEnergyRelease.jl
@@ -27,18 +27,21 @@
 function StrainEnergyRelease(K1,K2,K3,G,ν)
 
 
-
-#Wiki G-Criterion
+#=
+#Wiki G-Criterion - Crack extension force
 E=(2.0*G)*(1.0+ν);
 Eprime=E/(1-ν^2)
 fracturetoughness=sqrt.((abs.(K1).^2)+(abs.(K2).^2)+((Eprime/(2*G))*(abs.(K3).^2)));
+=#
 
-##Tada G criterion
-#Eprime=E/(1-ν^2)
-#G1=(abs.(K1).^2)./Eprime
-#G2=(abs.(K2).^2)./Eprime
-#G3=(abs.(K3).^2)./2*G
-#StrainEnergy=G1+G2+G3
+
+#Tada G criterion p1.7 - Crack extension force
+Eprime=E/(1-ν^2)
+G1=(abs.(K1).^2)./Eprime
+G2=(abs.(K2).^2)./Eprime
+G3=(abs.(K3).^2)./2*G
+fracturetoughness=G1+G2+G3
+
 
 ##Previous (same as wiki but simple )
 #StrainEnergy=((abs.(K1).^2+abs.(K2).^2).*(1-ν)+abs.(K3).^2)./(2*G);
