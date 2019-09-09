8c557432ede55fe4b1578a38354450fde3b80957
Tim Davis <davis@gfz-potsdam.de>
2019-09-09 17:24:30 +0200

    Update StrainEnergyRelease.jl

--- src/StrainEnergyRelease.jl
+++ src/StrainEnergyRelease.jl
@@ -36,6 +36,7 @@ fracturetoughness=sqrt.((abs.(K1).^2)+(abs.(K2).^2)+((Eprime/(2*G))*(abs.(K3).^2
 
 
 #Tada G criterion p1.7 - Crack extension force
+E=(2.0*G)*(1.0+ν);
 Eprime=E/(1-ν^2)
 G1=(abs.(K1).^2)./Eprime
 G2=(abs.(K2).^2)./Eprime
