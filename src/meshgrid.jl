edc8f645e4bcfa359c3dbb0d148568efd7816c2c
Tim Davis <davis@gfz-potsdam.de>
2020-10-26 11:02:46 +0100

    3D version of meshgrid func

--- src/meshgrid.jl
+++ src/meshgrid.jl
@@ -1,13 +1,34 @@
 function meshgrid(x,y)
 #Assuming vectors are both cols. Similar to MATLABS meshgrid func. 
 
+dimx=length(x);
 dimy=length(y);
-x=x';
+x=reshape(x,1,dimx)
 
 #Like repmat
-y=repeat(y, 1,length(x));
 x=repeat(x,dimy,1);
+y=repeat(y,1,dimx);
 
 return(x,y)
+
+end
+
+
+function meshgrid(x,y,z)
+#In 3D. Similar to MATLABS meshgrid func. 
+
+dimx=length(x);
+dimy=length(y);
+dimz=length(z);
+x=reshape(x,1,dimx,1)
+z=reshape(z,1,1,dimz)
+
+#Like repmat
+x=repeat(x, dimy,1,dimz);
+y=repeat(y, 1,dimx,dimz);
+z=repeat(z, dimy,dimx,1);
+#z=reshape(z,dimx,dimy,dimz)
+
+return(x,y,z)
 
 end
