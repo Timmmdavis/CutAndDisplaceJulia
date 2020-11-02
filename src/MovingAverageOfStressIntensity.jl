64f2c2431f96c5c6f155611947c25cb29c13aca7
Tim Davis <davis@gfz-potsdam.de>
2020-11-02 18:20:30 +0100

    Now smoothing all K values

--- src/MovingAverageOfStressIntensity.jl
+++ src/MovingAverageOfStressIntensity.jl
@@ -16,6 +16,21 @@ if avgeverynth==3
 	InnerPointOld    =[NaN NaN NaN] 
 	BackPoint    =[NaN NaN NaN] 
 	lps=0
+
+	FeP2P3S_StrainEnergy=copy(FeP2P3S.StrainEnergy)
+	FeP2P3S_K1=copy(FeP2P3S.K1)
+	FeP2P3S_K2=copy(FeP2P3S.K2)
+	FeP2P3S_K3=copy(FeP2P3S.K3)
+
+	FeP1P3S_StrainEnergy=copy(FeP1P3S.StrainEnergy)
+	FeP1P3S_K1=copy(FeP1P3S.K1)
+	FeP1P3S_K2=copy(FeP1P3S.K2)
+	FeP1P3S_K3=copy(FeP1P3S.K3)
+
+	FeP1P2S_StrainEnergy=copy(FeP1P2S.StrainEnergy)
+	FeP1P2S_K1=copy(FeP1P2S.K1)	
+	FeP1P2S_K2=copy(FeP1P2S.K2)	
+	FeP1P2S_K3=copy(FeP1P2S.K3)	
 	#For each edge loop
 	for i=1:length(UniqueEdges)
 	  b=vec(UniqueEdges[i])
@@ -29,46 +44,79 @@ if avgeverynth==3
 	  	elseif j==b[end]
 	  		j_future=b[1]
 	  	end
-
 	    #Extract the points on the current bit of the edge
 	    (k,Idx_trailing) =GrabPointNew6(InnerPoints,P1,P2,P3,j_trailing)
 	    if k==1
-	    	Vtrailing=FeP2P3S.StrainEnergy[Idx_trailing]
+	    	Vtrailing=FeP2P3S_StrainEnergy[Idx_trailing]
+	    	VtrailingK1=FeP2P3S_K1[Idx_trailing]
+	    	VtrailingK2=FeP2P3S_K2[Idx_trailing]
+	    	VtrailingK3=FeP2P3S_K3[Idx_trailing]
 	    elseif k==2
-	    	Vtrailing=FeP1P3S.StrainEnergy[Idx_trailing]
+	    	Vtrailing=FeP1P3S_StrainEnergy[Idx_trailing]
+	    	VtrailingK1=FeP1P3S_K1[Idx_traili_g]
+	    	VtrailingK2=FeP1P3S_K2[Idx_trailing]
+	    	VtrailingK3=FeP1P3S_K3[Idx_trailing]	    	
 	    elseif k==3
-	    	Vtrailing=FeP1P2S.StrainEnergy[Idx_trailing]
+	    	Vtrailing=FeP1P2S_StrainEnergy[Idx_trailing]
+	    	VtrailingK1=FeP1P2S_K1[Idx_trailing]
+	    	VtrailingK2=FeP1P2S_K2[Idx_trailing]
+	    	VtrailingK3=FeP1P2S_K3[Idx_trailing]		    	
 	    end    	
 	    (k,Idx_future) =GrabPointNew6(InnerPoints,P1,P2,P3,j_future)
 		if k==1
-	    	Vfuture=FeP2P3S.StrainEnergy[Idx_future]
+	    	Vfuture=FeP2P3S_StrainEnergy[Idx_future]
+	    	VfutureK1=FeP2P3S_K1[Idx_future]
+	    	VfutureK2=FeP2P3S_K2[Idx_future]
+	    	VfutureK3=FeP2P3S_K3[Idx_future]	    	
 	    elseif k==2
-	    	Vfuture=FeP1P3S.StrainEnergy[Idx_future]
+	    	Vfuture=FeP1P3S_StrainEnergy[Idx_future]
+	    	VfutureK1=FeP1P3S_K1[Idx_future]
+	    	VfutureK2=FeP1P3S_K2[Idx_future]
+	    	VfutureK3=FeP1P3S_K3[Idx_future]		    	
 	    elseif k==3
-	    	Vfuture=FeP1P2S.StrainEnergy[Idx_future]
+	    	Vfuture=FeP1P2S_StrainEnergy[Idx_future]
+	    	VfutureK1=FeP1P2S_K1[Idx_future]
+	    	VfutureK2=FeP1P2S_K2[Idx_future]
+	    	VfutureK3=FeP1P2S_K3[Idx_future]		    	
 	    end   
 	    (k,Idx_current) =GrabPointNew6(InnerPoints,P1,P2,P3,j_current)
 		if k==1
-			avg=(Vtrailing+FeP2P3S.StrainEnergy[Idx_current]+Vfuture)/3;
+			avg=(Vtrailing+FeP2P3S_StrainEnergy[Idx_current]+Vfuture)/3;
+			avgK1=(VtrailingK1+FeP2P3S_K1[Idx_current]+VfutureK1)/3;
+			avgK2=(VtrailingK2+FeP2P3S_K2[Idx_current]+VfutureK2)/3;
+			avgK3=(VtrailingK3+FeP2P3S_K3[Idx_current]+VfutureK3)/3;
 			if isnan(avg)
 				continue
 			end
 			FeP2P3S.StrainEnergy[Idx_current]=avg
+			FeP2P3S.K1[Idx_current]=avgK1
+			FeP2P3S.K2[Idx_current]=avgK2
+			FeP2P3S.K3[Idx_current]=avgK3
 	    elseif k==2
-	    	avg=(Vtrailing+FeP1P3S.StrainEnergy[Idx_current]+Vfuture)/3
+	    	avg=(Vtrailing+FeP1P3S_StrainEnergy[Idx_current]+Vfuture)/3
+			avgK1=(VtrailingK1+FeP1P3S_K1[Idx_current]+VfutureK1)/3;
+			avgK2=(VtrailingK2+FeP1P3S_K2[Idx_current]+VfutureK2)/3;
+			avgK3=(VtrailingK3+FeP1P3S_K3[Idx_current]+VfutureK3)/3;	    	
 			if isnan(avg)
 				continue
 			end
 	    	FeP1P3S.StrainEnergy[Idx_current]=avg
+			FeP1P3S.K1[Idx_current]=avgK1
+			FeP1P3S.K2[Idx_current]=avgK2
+			FeP1P3S.K3[Idx_current]=avgK3	    	
 	    elseif k==3
-	    	avg=(Vtrailing+FeP1P2S.StrainEnergy[Idx_current]+Vfuture)/3    	
+	    	avg=(Vtrailing+FeP1P2S_StrainEnergy[Idx_current]+Vfuture)/3 
+	    	avgK1=(VtrailingK1+FeP1P2S_K1[Idx_current]+VfutureK1)/3;
+			avgK2=(VtrailingK2+FeP1P2S_K2[Idx_current]+VfutureK2)/3;
+			avgK3=(VtrailingK3+FeP1P2S_K3[Idx_current]+VfutureK3)/3;
 	    	if isnan(avg)
 				continue
 			end
-	    	FeP1P2S.StrainEnergy[Idx_current]=avg   	
+	    	FeP1P2S.StrainEnergy[Idx_current]=avg  
+	    	FeP1P2S.K1[Idx_current]=avgK1
+			FeP1P2S.K2[Idx_current]=avgK2
+			FeP1P2S.K3[Idx_current]=avgK3	
 	    end       
-	    
-
 	    #@info Idx_trailing Idx_current Idx_future
 	    end
 	end 
@@ -86,6 +134,22 @@ if avgeverynth==5
 	InnerPointOld    =[NaN NaN NaN] 
 	BackPoint    =[NaN NaN NaN] 
 	lps=0
+
+	FeP2P3S_StrainEnergy=copy(FeP2P3S.StrainEnergy)
+	FeP2P3S_K1=copy(FeP2P3S.K1)
+	FeP2P3S_K2=copy(FeP2P3S.K2)
+	FeP2P3S_K3=copy(FeP2P3S.K3)
+
+	FeP1P3S_StrainEnergy=copy(FeP1P3S.StrainEnergy)
+	FeP1P3S_K1=copy(FeP1P3S.K1)
+	FeP1P3S_K2=copy(FeP1P3S.K2)
+	FeP1P3S_K3=copy(FeP1P3S.K3)
+
+	FeP1P2S_StrainEnergy=copy(FeP1P2S.StrainEnergy)
+	FeP1P2S_K1=copy(FeP1P2S.K1)	
+	FeP1P2S_K2=copy(FeP1P2S.K2)	
+	FeP1P2S_K3=copy(FeP1P2S.K3)	
+
 	#For each edge loop
 	for i=1:length(UniqueEdges)
 	  b=vec(UniqueEdges[i])
@@ -112,63 +176,114 @@ if avgeverynth==5
 	    #Extract the points on the current bit of the edge
 	    (k,Idx_trailing) =GrabPointNew6(InnerPoints,P1,P2,P3,j_trailing)
 	    if k==1
-	    	Vtrailing=FeP2P3S.StrainEnergy[Idx_trailing]
+	    	Vtrailing=FeP2P3S_StrainEnergy[Idx_trailing]
+	    	VtrailingK1=FeP2P3S_K1[Idx_trailing]
+	    	VtrailingK2=FeP2P3S_K2[Idx_trailing]
+	    	VtrailingK3=FeP2P3S_K3[Idx_trailing]
 	    elseif k==2
-	    	Vtrailing=FeP1P3S.StrainEnergy[Idx_trailing]
+	    	Vtrailing=FeP1P3S_StrainEnergy[Idx_trailing]
+	    	VtrailingK1=FeP1P3S_K1[Idx_traili_g]
+	    	VtrailingK2=FeP1P3S_K2[Idx_trailing]
+	    	VtrailingK3=FeP1P3S_K3[Idx_trailing]	    	
 	    elseif k==3
-	    	Vtrailing=FeP1P2S.StrainEnergy[Idx_trailing]
+	    	Vtrailing=FeP1P2S_StrainEnergy[Idx_trailing]
+	    	VtrailingK1=FeP1P2S_K1[Idx_trailing]
+	    	VtrailingK2=FeP1P2S_K2[Idx_trailing]
+	    	VtrailingK3=FeP1P2S_K3[Idx_trailing]
 	    end   
 		(k,Idx_trailing) =GrabPointNew6(InnerPoints,P1,P2,P3,j_trailing2)
 	    if k==1
-	    	Vtrailing2=FeP2P3S.StrainEnergy[Idx_trailing]
+	    	Vtrailing_2=FeP2P3S_StrainEnergy[Idx_trailing]
+	    	VtrailingK1_2=FeP2P3S_K1[Idx_trailing]
+	    	VtrailingK2_2=FeP2P3S_K2[Idx_trailing]
+	    	VtrailingK3_2=FeP2P3S_K3[Idx_trailing]
 	    elseif k==2
-	    	Vtrailing2=FeP1P3S.StrainEnergy[Idx_trailing]
+	    	Vtrailing_2=FeP1P3S_StrainEnergy[Idx_trailing]
+	    	VtrailingK1_2=FeP1P3S_K1[Idx_traili_g]
+	    	VtrailingK2_2=FeP1P3S_K2[Idx_trailing]
+	    	VtrailingK3_2=FeP1P3S_K3[Idx_trailing]	    	
 	    elseif k==3
-	    	Vtrailing2=FeP1P2S.StrainEnergy[Idx_trailing]
+	    	Vtrailing_2=FeP1P2S_StrainEnergy[Idx_trailing]
+	    	VtrailingK1_2=FeP1P2S_K1[Idx_trailing]
+	    	VtrailingK2_2=FeP1P2S_K2[Idx_trailing]
+	    	VtrailingK3_2=FeP1P2S_K3[Idx_trailing]
 	    end   
 	    (k,Idx_future) =GrabPointNew6(InnerPoints,P1,P2,P3,j_future)
 		if k==1
-	    	Vfuture=FeP2P3S.StrainEnergy[Idx_future]
+			Vfuture=FeP2P3S_StrainEnergy[Idx_future]
+	    	VfutureK1=FeP2P3S_K1[Idx_future]
+	    	VfutureK2=FeP2P3S_K2[Idx_future]
+	    	VfutureK3=FeP2P3S_K3[Idx_future]	    	
 	    elseif k==2
-	    	Vfuture=FeP1P3S.StrainEnergy[Idx_future]
+	    	Vfuture=FeP1P3S_StrainEnergy[Idx_future]
+	    	VfutureK1=FeP1P3S_K1[Idx_future]
+	    	VfutureK2=FeP1P3S_K2[Idx_future]
+	    	VfutureK3=FeP1P3S_K3[Idx_future]		    	
 	    elseif k==3
-	    	Vfuture=FeP1P2S.StrainEnergy[Idx_future]
+	    	Vfuture=FeP1P2S_StrainEnergy[Idx_future]
+	    	VfutureK1=FeP1P2S_K1[Idx_future]
+	    	VfutureK2=FeP1P2S_K2[Idx_future]
+	    	VfutureK3=FeP1P2S_K3[Idx_future]		    	
 	    end   
 	    (k,Idx_future) =GrabPointNew6(InnerPoints,P1,P2,P3,j_future2)
 		if k==1
-	    	Vfuture2=FeP2P3S.StrainEnergy[Idx_future]
+			Vfuture_2=FeP2P3S_StrainEnergy[Idx_future]
+	    	VfutureK1_2=FeP2P3S_K1[Idx_future]
+	    	VfutureK2_2=FeP2P3S_K2[Idx_future]
+	    	VfutureK3_2=FeP2P3S_K3[Idx_future]	    	
 	    elseif k==2
-	    	Vfuture2=FeP1P3S.StrainEnergy[Idx_future]
+	    	Vfuture_2=FeP1P3S_StrainEnergy[Idx_future]
+	    	VfutureK1_2=FeP1P3S_K1[Idx_future]
+	    	VfutureK2_2=FeP1P3S_K2[Idx_future]
+	    	VfutureK3_2=FeP1P3S_K3[Idx_future]		    	
 	    elseif k==3
-	    	Vfuture2=FeP1P2S.StrainEnergy[Idx_future]
+	    	Vfuture_2=FeP1P2S_StrainEnergy[Idx_future]
+	    	VfutureK1_2=FeP1P2S_K1[Idx_future]
+	    	VfutureK2_2=FeP1P2S_K2[Idx_future]
+	    	VfutureK3_2=FeP1P2S_K3[Idx_future]	
 	    end   	    
 	    (k,Idx_current) =GrabPointNew6(InnerPoints,P1,P2,P3,j_current)
 		if k==1
-			avg=(Vtrailing2+Vtrailing+FeP2P3S.StrainEnergy[Idx_current]+Vfuture+Vfuture2)/5;
+			avg=(Vtrailing_2+Vtrailing+FeP2P3S_StrainEnergy[Idx_current]+Vfuture+Vfuture_2)/5;
+			avgK1=(VtrailingK1_2+VtrailingK1+FeP2P3S_K1[Idx_current]+VfutureK1+VfutureK1_2)/5;
+			avgK2=(VtrailingK2_2+VtrailingK2+FeP2P3S_K2[Idx_current]+VfutureK2+VfutureK2_2)/5;
+			avgK3=(VtrailingK3_2+VtrailingK3+FeP2P3S_K3[Idx_current]+VfutureK3+VfutureK3_2)/5;
 			if isnan(avg)
 				continue
 			end
 			FeP2P3S.StrainEnergy[Idx_current]=avg
+			FeP2P3S.K1[Idx_current]=avgK1
+			FeP2P3S.K2[Idx_current]=avgK2
+			FeP2P3S.K3[Idx_current]=avgK3
 	    elseif k==2
-	    	avg=(Vtrailing2+Vtrailing+FeP1P3S.StrainEnergy[Idx_current]+Vfuture+Vfuture2)/5
+			avg=(Vtrailing_2+Vtrailing+FeP1P3S_StrainEnergy[Idx_current]+Vfuture+Vfuture_2)/5;
+			avgK1=(VtrailingK1_2+VtrailingK1+FeP1P3S_K1[Idx_current]+VfutureK1+VfutureK1_2)/5;
+			avgK2=(VtrailingK2_2+VtrailingK2+FeP1P3S_K2[Idx_current]+VfutureK2+VfutureK2_2)/5;
+			avgK3=(VtrailingK3_2+VtrailingK3+FeP1P3S_K3[Idx_current]+VfutureK3+VfutureK3_2)/5;
 			if isnan(avg)
 				continue
 			end
-	    	FeP1P3S.StrainEnergy[Idx_current]=avg
+			FeP1P3S.StrainEnergy[Idx_current]=avg
+			FeP1P3S.K1[Idx_current]=avgK1
+			FeP1P3S.K2[Idx_current]=avgK2
+			FeP1P3S.K3[Idx_current]=avgK3
 	    elseif k==3
-	    	avg=(Vtrailing2+Vtrailing+FeP1P2S.StrainEnergy[Idx_current]+Vfuture+Vfuture2)/5   	
-	    	if isnan(avg)
+			avg=(Vtrailing_2+Vtrailing+FeP1P2S_StrainEnergy[Idx_current]+Vfuture+Vfuture_2)/5;
+			avgK1=(VtrailingK1_2+VtrailingK1+FeP1P2S_K1[Idx_current]+VfutureK1+VfutureK1_2)/5;
+			avgK2=(VtrailingK2_2+VtrailingK2+FeP1P2S_K2[Idx_current]+VfutureK2+VfutureK2_2)/5;
+			avgK3=(VtrailingK3_2+VtrailingK3+FeP1P2S_K3[Idx_current]+VfutureK3+VfutureK3_2)/5;
+			if isnan(avg)
 				continue
 			end
-	    	FeP1P2S.StrainEnergy[Idx_current]=avg
+			FeP1P2S.StrainEnergy[Idx_current]=avg
+			FeP1P2S.K1[Idx_current]=avgK1
+			FeP1P2S.K2[Idx_current]=avgK2
+			FeP1P2S.K3[Idx_current]=avgK3
 	    end       
-	    
-
 	    #@info Idx_trailing Idx_current Idx_future
 	    end
 	end 
 end
-
 
 return FeP1P2S,FeP1P3S,FeP2P3S
 
--- src/StrainEnergyRelease.jl
+++ src/StrainEnergyRelease.jl
@@ -26,15 +26,18 @@
 
 function StrainEnergyRelease(K1,K2,K3,G,ν)
 
+fracturetoughness=sqrt.((abs.(K1).^2)+(abs.(K2).^2)+((1/(1-ν))*(abs.(K3).^2)));
+#=
 E=(2.0*G)*(1.0+ν);
 Eprime=E/(1-(ν^2))
+=#
 
 #=
 #Wiki G-Criterion - stress intensity factor page
 fracturetoughness=sqrt.((abs.(K1).^2)+(abs.(K2).^2)+((Eprime/(2*G))*(abs.(K3).^2)));
 =#
 
-
+#=
 #Tada G criterion p1.7 - Crack extension force
 #eq 20 tada
 G1=(abs.(K1).^2)./Eprime
@@ -45,7 +48,7 @@ StrainEnergy=G1.+G2.+G3 #energy release rate (strain energy)
 #Sih Macdonald - Fracture mechaincs applied to engineering problems - strain energy density criterion
 #Base page 364 - Gc to KIc
 fracturetoughness=sqrt.((StrainEnergy.*E)./(1-(ν^2)))
-
+=#
 
 ##Previous (same as wiki but simple )
 #StrainEnergy=((abs.(K1).^2+abs.(K2).^2).*(1-ν)+abs.(K3).^2)./(2*G);
